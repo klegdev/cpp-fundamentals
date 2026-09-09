9d8fcc2cd095ca83c1d3465bcc6629010a111bae
Konrad Legutko <konrad.legutko2@gmail.com>
2026-09-09 19:41:57 +0000

    [feat] Implementation maxOfVector

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,10 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    int max = vec.size() > 0 ? vec[0] : 0;
+    for (size_t i = 1; i < vec.size(); i++) {
+        if (vec[i] > max)
+            max = vec[i];
+    }
+    return max;
 }
