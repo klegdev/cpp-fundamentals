e674ad9893a1442870ede77294b6a230ffbd199e
Konrad Legutko <konrad.legutko2@gmail.com>
2026-09-09 20:09:27 +0000

    [feat] Implementation generateSequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,13 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if(count <= 0)
+        return std::vector<int>{};
+    int sum = step;
+    std::vector<int> sequence = {sum};
+    for (size_t i = 1; i < count; i++) {
+        sum += step;
+        sequence.push_back(sum);
+    }
+    return sequence;
 }
