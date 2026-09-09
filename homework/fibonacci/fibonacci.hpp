6b44cada4daae6e9c15526a792157d223be14bf0
Konrad Legutko <konrad.legutko2@gmail.com>
2026-09-09 20:33:17 +0000

    [feat] Implementation fibonacci

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,22 @@
 #pragma once
+#include <cstddef>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence < 1)
+        return 0;
+    int a = 0;
+    int b = 1;
+    for (size_t i = 0; i < sequence; i++) {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return a;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence <= 1)
+        return sequence;
+    else
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
